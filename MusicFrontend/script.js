08be5045a5198c1c7c42942059bbf69d9349a920
Madhur Rastogi <madhurrastogi20@gmail.com>
2026-01-15 20:05:36 +0000

    remove cli tailwind and add play cdn

--- MusicFrontend/script.js
+++ MusicFrontend/script.js
@@ -2,17 +2,20 @@ const songs=[
     {
         "title":"SongOne",
         "artist":"Artist1",
-        "src":"Library/Songs/Song1.mp3"
+        "src":"Library/Songs/Song1.mp3",
+        "cover":"https://th.bing.com/th/id/OIP.lulXuCXRYazVF30Q-8J1ZwHaHa?w=161&h=180&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3"
     },
     {
         "title":"SongTwo",
-        "artist":"Arist2",
-        "src":"Library/Songs/Song2.mp3"
+        "artist":"Artist2",
+        "src":"Library/Songs/song2.mp3",
+        "cover":"https://ilyrictranslation.com/wp-content/uploads/2025/07/SIRRA-LYRICS-1024x576.webp"
     },
     {
         "title":"SongOne",
         "artist":"Artist1",
-        "src":"Library/Songs/Song1.mp3"
+        "src":"Library/Songs/Song1.mp3",
+        "cover":"https://th.bing.com/th/id/OIP.lulXuCXRYazVF30Q-8J1ZwHaHa?w=161&h=180&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3"
     }
 ];
 
@@ -21,6 +24,11 @@ const prev=document.getElementById("prev");
 const next=document.getElementById("next");
 const play=document.getElementById("play");
 
+const cover=document.querySelector(".cover");
+const currentTime=document.querySelector(".current-time");
+const duration=document.querySelector(".duration");
+const volumeSlider=document.querySelector(".volume-slider")
+const progress=document.querySelector(".progress")
 const title=document.querySelector(".title");
 const artist=document.querySelector(".artist");
 
@@ -32,9 +40,17 @@ function UIBinding(index)
     const song=songs[index];
     title.textContent=song.title;
     artist.textContent=song.artist;
-
+    cover.src=song.cover
     audio.src=song.src;
 }
+
+function formatTime(time)
+{
+    const min=Math.floor(time/60);
+    const sec=Math.floor(time%60);
+    return `${min}:${sec<10 ? "0":""}${sec}`;
+}
+
 play.addEventListener("click",()=>{
     if(!isPlaying)
     {
@@ -75,7 +91,6 @@ next.addEventListener("click",()=>{
     play.textContent="⏸";
 });
 
-
 audio.addEventListener("ended",()=>{
     currentInd++;
     if(currentInd>=songs.length)
@@ -86,5 +101,24 @@ audio.addEventListener("ended",()=>{
     audio.play();
 });
 
+audio.addEventListener("timeupdate",()=>{
+    if(!audio.duration) return;
+    progress.value=(audio.currentTime/audio.duration)*100;
+});
+
+progress.addEventListener("input",()=>{
+    audio.currentTime=(progress.value/100)*audio.duration;
+});
+
+volumeSlider.addEventListener("input",()=>{
+    audio.volume=volumeSlider.value;
+});
+
+audio.addEventListener("loadedmetadata",()=>{
+    duration.textContent=formatTime(audio.duration);
+});
+audio.addEventListener("timeupdate",()=>{
+    currentTime.textContent=formatTime(audio.currentTime);
+});
 
 UIBinding(currentInd);
